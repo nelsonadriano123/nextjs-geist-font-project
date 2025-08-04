10d728b5c3cbf7fe30ceeb2fc08802f4c44aa002
nelsonadriano123 <60528611+nelsonadriano123@users.noreply.github.com>
2025-08-03 23:59:37 -0500

    Add .next/static/chunks/src_app_services_page_tsx_a10bebae._.js 1754283576887

---
+++ .next/static/chunks/src_app_services_page_tsx_a10bebae._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_services_page_tsx_bd255872._.js"
+],
+    source: "dynamic"
+});
